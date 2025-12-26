b3fd969ff2fec692a935422e5708750d0c64377a
SAP-SHARKS <support@sapsharks.com>
2025-12-26 17:10:18 +0500

    Do

--- components/MemorialList.tsx
+++ components/MemorialList.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import { supabase } from '../services/supabaseClient';
 
 const MemorialList: React.FC<{ onSelectProfile: (profile: any) => void }> = ({ onSelectProfile }) => {
